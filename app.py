e09c913653410e1b5cfac0b895139aa1241eff83
kristinoktvn <82004668+kristinoktvn@users.noreply.github.com>
2024-06-06 09:56:57 +0700

    terbaru

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def generate_prima():
 
     for num in range (2, number + 1):
         prim = True
-        for i in range (2, int(num*0.5) + 1):
+        for i in range (2, int(num**0.5) + 1):
             if num % 1 == 0:
                 prim = False
                 break 
